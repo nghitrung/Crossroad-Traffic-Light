27d63e1eaa6fb6d2e39de11fc4d904cef0dd1e8d
nghitrung <nguyenhieutrung1405@gmail.com>
2026-04-03 06:22:28 +0000

    add source code

---
+++ source/top_module.sv
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+module top_module(
+    input logic clk,
+    input logic rst,
+    input logic emer_1,
+    input logic emer_2,
+    output logic [2:0] LED_1,
+    output logic [2:0] LED_2
+);
+
+    state_t curr_state;
+    logic [4:0] timer1, timer2;
+    
+    state_of_light SOL (
+        .clk(clk),
+        .rst(rst),
+        .emer_1(emer_1),
+        .emer_2(emer_2),
+        .state_signal(curr_state)
+    );
+    
+    // LED 1 (Green -> Yellow -> Red)
+    traffic_light #(.light_num(1'b1)) TFL1(
+        .clk(clk),
+        .rst(rst),
+        .curr_state(curr_state),
+        .timer(timer1),
+        .led(LED_1)
+    );
+    
+    traffic_light #(.light_num(1'b0)) TFL2(
+        .clk(clk),
+        .rst(rst),
+        .curr_state(curr_state),
+        .timer(timer2),
+        .led(LED_2)
+    );
+    
+endmodule
---
+++ source/traffic_light.sv
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+
+typedef enum logic [2:0] {
+    OFF =    3'b000,
+    RED =    3'b001,
+    GREEN =  3'b010,
+    YELLOW = 3'b011
+} color_t;
+
+module traffic_light #(
+    parameter bit light_num = 0, // 1 for light 1, 0 for light 2
+    parameter logic [4:0] RED_DUTY = 6,
+    parameter logic [4:0] GREEN_DUTY = 3,
+    parameter logic [4:0] YELLOW_DUTY = 3 
+)(
+
+    input logic clk,
+    input logic rst,
+    input state_t curr_state,
+    output logic [4:0] timer,
+    output color_t led
+);
+    
+
+    localparam logic [4:0] DUTY_CYCLE = RED_DUTY + GREEN_DUTY + YELLOW_DUTY; 
+    
+    always_ff @(posedge clk or negedge rst) begin
+        if (!rst) timer <= 0;
+        else if (timer >= DUTY_CYCLE - 1) timer <= 0;
+        else timer <= timer + 1;
+    end
+    
+    always_comb begin
+        if (curr_state == EMER_1) led = light_num ? RED : GREEN;
+        else if (curr_state == EMER_2) led = light_num ? GREEN : RED;
+        else begin 
+        // light 1 GREEN -> YELLOW -> GREEN
+            if (!light_num) begin
+                if (timer < GREEN_DUTY) led = GREEN;
+                else if (timer < (YELLOW_DUTY + GREEN_DUTY)) led = YELLOW;
+                else led = RED;
+            end else begin 
+                if (timer < RED_DUTY) led = RED;
+                else if (timer < (RED_DUTY + GREEN_DUTY)) led = GREEN;
+                else led = YELLOW;
+            end
+        end
+    end
+endmodule
